6eba49f2b5e85a31b6ab0776579a3f7f1759d762
Tinaynox <tinaynox@gmail.com>
2023-06-12 21:51:47 +0300

    Textures rescale test

---
+++ tools/scripts/assets/tests/win/assembly-assets-x0.5-nc.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call %~dp0\var.bat
+
+set ASSETS_X1_DIR=%PRJ_ROOT_DIR%\%PROJECT_NAME%\resources\resources-x0.5\data\
+call python %MAZE_ENGINE_DIR%\..\tools\scripts\assets\assembly_resources.py  --src %ASSETS_DIR% --dst %ASSETS_X1_DIR% --tscales 0.5
+
+set ASSETS_X1_NC_DIR=%PRJ_ROOT_DIR%\%PROJECT_NAME%\resources\resources-x0.5-nc\data\
+call python %MAZE_ENGINE_DIR%\..\tools\scripts\assets\make_data.py --src %ASSETS_X1_DIR% --dst %ASSETS_X1_NC_DIR% --tools %MAZE_ENGINE_DIR%\..\third-party\tools --temp %PRJ_ROOT_DIR%\%PROJECT_NAME%\resources\.temp --cache cache-win-x0.5-nc.pkl
