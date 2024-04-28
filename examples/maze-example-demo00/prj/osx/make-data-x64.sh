3a03a25bee9d4cde8c43cf735d5b33b513027e16
Tinaynox <n.d.a@mail.ru>
2024-04-28 16:30:08 +0300

    OSX examples fixed

--- examples/maze-example-demo00/prj/osx/make-data-x64.sh
+++ examples/maze-example-demo00/prj/osx/make-data-x64.sh
@@ -4,7 +4,7 @@ cd $SCRIPT_PATH
 source var.sh
 
 
-RESOURCES_DIR=$../../../resources
+RESOURCES_DIR=$SCRIPT_PATH/../../resources
 source ../../../templates/prj/osx/make-data-x64.sh
 
 read -s -n 1 -p "Press any key to continue..."
