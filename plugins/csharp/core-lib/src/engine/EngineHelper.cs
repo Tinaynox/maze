0794c9f7aca73f219b0f96db6696c16a9d85b592
Tinaynox <tinaynox@gmail.com>
2025-06-07 14:54:38 +0300

    Progress

--- plugins/csharp/core-lib/src/engine/EngineHelper.cs
+++ plugins/csharp/core-lib/src/engine/EngineHelper.cs
@@ -12,6 +12,9 @@ namespace Maze.Engine
             bool additive = true,
             sbyte ecsWorldId = -1)
         {
+            if (renderTarget == null)
+                throw new ArgumentException("RenderTarget is null!");
+
             int sceneId = InternalCalls.LoadAssetScene(sceneName, renderTarget.ResourceId, additive, ecsWorldId);
             return new EcsScene(sceneId);
         }
@@ -22,6 +25,9 @@ namespace Maze.Engine
             bool additive,
             EcsWorld ecsWorld)
         {
+            if (ecsWorld == null)
+                throw new ArgumentException("ECS World is null!");
+
             return LoadAssetScene(sceneName, renderTarget, additive, ecsWorld.Id);
         }
 
