d4def8afaa1d0bfa2482a6f789ef34a051779019
Tinaynox <tinaynox@gmail.com>
2026-01-24 22:02:45 +0200

    Progress

--- plugins/csharp/core-lib/src/core/math/AABB2D.cs
+++ plugins/csharp/core-lib/src/core/math/AABB2D.cs
@@ -7,6 +7,8 @@ namespace Maze.Core
     [StructLayout(LayoutKind.Sequential)]
     public struct AABB2D
     {
+        public static AABB2D Zero => new AABB2D();
+
         public float MinX;
         public float MinY;
         public float MaxX;
@@ -22,6 +24,40 @@ namespace Maze.Core
             MinY = minY;
             MaxX = maxX;
             MaxY = maxY;
+        }
+
+        public static bool operator ==(AABB2D value0, AABB2D value1)
+        {
+            return
+                value0.MinX == value1.MinX && value0.MinY == value1.MinY &&
+                value0.MaxX == value1.MaxX && value0.MaxY == value1.MaxY;
+        }
+
+        public static bool operator !=(AABB2D value0, AABB2D value1)
+        {
+            return
+                value0.MinX != value1.MinX || value0.MinY != value1.MinY ||
+                value0.MaxX != value1.MaxX || value0.MaxY != value1.MaxY;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is AABB2D other)
+                return this == other;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked // Handle arithmetic overflow
+            {
+                int hash = 17;
+                hash = hash * 23 + MinX.GetHashCode();
+                hash = hash * 23 + MinY.GetHashCode();
+                hash = hash * 23 + MaxX.GetHashCode();
+                hash = hash * 23 + MaxY.GetHashCode();
+                return hash;
+            }
         }
 
         public override string ToString()
--- plugins/csharp/core-lib/src/core/math/AABB3D.cs
+++ plugins/csharp/core-lib/src/core/math/AABB3D.cs
@@ -7,6 +7,8 @@ namespace Maze.Core
     [StructLayout(LayoutKind.Sequential)]
     public struct AABB3D
     {
+        public static AABB3D Zero => new AABB3D();
+
         public float MinX;
         public float MinY;
         public float MinZ;
@@ -28,6 +30,42 @@ namespace Maze.Core
             MaxX = maxX;
             MaxY = maxY;
             MaxZ = maxZ;
+        }
+
+        public static bool operator ==(AABB3D value0, AABB3D value1)
+        {
+            return
+                value0.MinX == value1.MinX && value0.MinY == value1.MinY && value0.MinZ == value1.MinZ &&
+                value0.MaxX == value1.MaxX && value0.MaxY == value1.MaxY && value0.MaxZ == value1.MaxZ;
+        }
+
+        public static bool operator !=(AABB3D value0, AABB3D value1)
+        {
+            return
+                value0.MinX != value1.MinX || value0.MinY != value1.MinY || value0.MinZ != value1.MinZ ||
+                value0.MaxX != value1.MaxX || value0.MaxY != value1.MaxY || value0.MaxZ != value1.MaxZ;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is AABB3D other)
+                return this == other;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked // Handle arithmetic overflow
+            {
+                int hash = 17;
+                hash = hash * 23 + MinX.GetHashCode();
+                hash = hash * 23 + MinY.GetHashCode();
+                hash = hash * 23 + MinZ.GetHashCode();
+                hash = hash * 23 + MaxX.GetHashCode();
+                hash = hash * 23 + MaxY.GetHashCode();
+                hash = hash * 23 + MaxZ.GetHashCode();
+                return hash;
+            }
         }
 
         public override string ToString()
