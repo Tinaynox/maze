c2e1c1946a2f575c4418099132b2188d269e7320
Tinaynox <tinaynox@gmail.com>
2026-03-09 14:00:06 +0200

    Progress

--- plugins/csharp/core-lib/src/graphics/ecs/SpriteRenderer2D.cs
+++ plugins/csharp/core-lib/src/graphics/ecs/SpriteRenderer2D.cs
@@ -4,11 +4,16 @@ namespace Maze.Graphics
 {
     public class SpriteRenderer2D : NativeComponent
     {
-
         public Material Material
         {
             get { InternalCalls.SpriteRenderer2DGetMaterial(NativeComponentPtr, out int resId); return new Material(resId); }
             set { InternalCalls.SpriteRenderer2DSetMaterial(NativeComponentPtr, value.ResourceId); }
+        }
+
+        public Sprite Sprite
+        {
+            get { InternalCalls.SpriteRenderer2DGetSprite(NativeComponentPtr, out int resId); return new Sprite(resId); }
+            set { InternalCalls.SpriteRenderer2DSetSprite(NativeComponentPtr, value.ResourceId); }
         }
 
         public SpriteRenderer2D(NativePtr nativeComponentPtr)
